c03fd7b349430bc2f987e99af5e71a73b85f1048
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-11-06 15:18:38 -0600

    starting conversion to database reading

---
+++ nommad/src/Components/Profile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+
+class Profile extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="profile">
+
+      </div>
+    );
+  }
+}
+
+export default Profile;
--- nommad/src/Components/Tile.js
+++ nommad/src/Components/Tile.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 class Tile extends Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      truck: this.props.truck[0]
+    }
   }
+
   render() {
+    let sectionStyle = {
+        width: "100%",
+        height: "100%",
+        backgroundImage: `url(${this.state.truck.image_url})`
+      };
     return (
-      <div className="tile">
-      <p>{this.props.truck}</p>
+      <div className="tile" style={ sectionStyle }>
+        <h3>{this.state.truck.name}</h3>
+
 
 
       </div>
@@ -16,3 +27,10 @@ class Tile extends Component {
 }
 
 export default Tile;
+
+
+// let tileImg = this.props.truck[0].image_url};
+// let sectionStyle = {
+//       backgroundImage: "url(" + {tileImg} + ")"
+//     };
+// console.log(this.props.truck);
