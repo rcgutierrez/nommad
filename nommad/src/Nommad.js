f6e8c903a81e35b8170bc212ffb4391f1be300ea
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-11-08 11:22:07 -0600

    adding getZipCode to Nommad component

--- nommad/src/Nommad.js
+++ nommad/src/Nommad.js
@@ -15,32 +15,44 @@ class Nommad extends Component {
       getRequestData: ''
     }
     this.getFoodTruck = this.getFoodTruck.bind(this);
-  }
-
-    componentDidMount() {
-      this.getFoodTruck();
-    }
-
-    getFoodTruck() {
-      this.setState({
-        isFetchingTrucks: true
-      })
-
-
-
-    const yelpApi = 'https://nommad-backend.herokuapp.com/api';
-    axios.get(yelpApi)
-         .then((response) => {
-           console.log(response);
-           let trucksArr = response.data.businesses;
-           this.setState({
-             getRequestData: trucksArr
-           })
-           //Re render it in UI
-         }).catch(error => {
-           console.log(`Error, ${error}`);
-         });
-    }
+    this.getZipCode = this.getZipCode.bind(this);
+      }
+        componentDidMount() {
+          this.getFoodTruck();
+        }
+        getFoodTruck() {
+          this.setState({
+            isFetchingTrucks: true
+          })
+          const yelpApi = 'https://nommad-backend.herokuapp.com/api';
+          axios.get(yelpApi)
+             .then((response) => {
+               console.log(response);
+               let trucksArr = response.data.businesses;
+               this.setState({
+                 getRequestData: trucksArr
+               })
+               //Re render it in UI
+             }).catch(error => {
+               console.log(`Error, ${error}`);
+          });
+        }
+        getZipCode() {
+          // console.log(this.state.getRequestData, ' request data');
+          let truckInfoArr = [];
+          let data = this.state.getRequestData;
+          let zip = document.querySelector("input").value;
+            console.log(zip, ' zip');
+            data.forEach(obj => {
+              // console.log(typeof obj.location.zip_code, ' objects')
+              if(obj.location.zip_code === zip) {
+                console.log(`Data match`)
+                truckInfoArr.push(obj);
+              }
+            })
+            console.log(truckInfoArr, ' data info')
+          return truckInfoArr;
+        }
 
   render() {
     let truckInfo = this.state.getRequestData;
