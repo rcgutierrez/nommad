681275921c920228ebd06d36c309bbf99fc728c5
Karla Moreno <moreno_karla@live.com>
2017-11-08 15:15:47 -0600

    changed background color, added yelp logo

--- nommad/src/Nommad.js
+++ nommad/src/Nommad.js
@@ -79,6 +79,7 @@ class Nommad extends Component {
 
         <footer className="clearfix">
           <p>Made with <span className="heart">♥︎</span> at General Assembly</p>
+          <img src={require('./images/yelp-logo.png')} id="yelp-logo" alt="yelp logo" />
         </footer>
       </div>
     );
